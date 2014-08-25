703bfca9b524fb84cc2a540fe877e4d410d801bc
Ric Allinson <capecodehq@yahoo.co.uk>
2014-08-25 14:38:18 -0700

    Big change of moving the main code into it's own library

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+    "github.com/ricallinson/mapr"
 )
 
 func main() {
 	var port = flag.String("port", "9090", "port to use for the server")
 	flag.Parse()
 	fmt.Printf("Server running on port %v\n", *port)
-	NewServer(*port)
+	mapr.NewServer(*port)
 }
--- main_test.go
+++ main_test.go
@@ -5,19 +5,20 @@ import (
 	"strconv"
 	"testing"
 	"time"
+    "github.com/ricallinson/mapr"
 )
 
 func TestServer(t *testing.T) {
 
 	// Create a Server for all tests to run against.
-	go NewServer("9090")
+	go mapr.NewServer("9090")
 	// Wait while the server starts.
 	time.Sleep(1 * time.Second)
 
 	Describe("Put() Get()", func() {
 		It("should return [true]", func() {
 			// Create a client.
-			c := NewClient("127.0.0.1:9090")
+			c := mapr.NewClient("127.0.0.1:9090")
 			// Put a Value
 			AssertEqual(c.Put("a", "b"), true)
 			// Get a Value
@@ -29,7 +30,7 @@ func TestServer(t *testing.T) {
 }
 
 func BenchmarkServer(b *testing.B) {
-	c := NewClient("127.0.0.1:9090")
+	c := mapr.NewClient("127.0.0.1:9090")
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		key := strconv.Itoa(i)
