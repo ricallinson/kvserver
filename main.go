3e4e58ea04929674216b525938699cd58934d7e9
Ric Allinson <capecodehq@yahoo.co.uk>
2014-08-25 20:56:32 -0700

    Updateto use new Mapr API

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	var port = flag.String("port", "9090", "port to use for the server")
 	flag.Parse()
 	fmt.Printf("Server running on port %v\n", *port)
-	mapr.NewServer(*port)
+	s := mapr.NewServer(*port)
+    s.Listen()
 }
